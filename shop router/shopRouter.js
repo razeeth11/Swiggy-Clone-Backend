631fd66db4d18ab6cb9ccee3c9d4dfdb87447df7
razeeth11 <razeeth011@gmail.com>
2023-03-29 09:13:29 +0530

    Router

--- shop router/shopRouter.js
+++ shop router/shopRouter.js
@@ -6,17 +6,7 @@ const router = express.Router();
 router.get("/", function (request, response) {
   response.send("hello");
 });
-// ------------------------------------------- get shopName by ID (API)  -------------------------------------------
-router.get("/shopDetails/:shopName", async function (request, response) {
-  const { shopName } = request.params;
 
-  const data = await client
-    .db("Swiggy")
-    .collection("product")
-    .findOne({ shopName: shopName });
-
-  response.send(data);
-});
 // ------------------------------------------- get All shop details (API)  -------------------------------------------
 router.get("/data", async function (request, response) {
   const data = await client
@@ -27,6 +17,19 @@ router.get("/data", async function (request, response) {
 
   response.send(data);
 });
+
+// ------------------------------------------- get shopName by shopName (API)  -------------------------------------------
+router.get("/shopDetails/:shopName", async function (request, response) {
+  const { shopName } = request.params;
+
+  const data = await client
+    .db("Swiggy")
+    .collection("product")
+    .findOne({ shopName: shopName });
+
+  response.send(data);
+});
+
 // ------------------------------------------- get shopDetails by Delivery Time (API)  -------------------------------------------
 router.get("/deliveryTime", async function (request, response) {
   const data = await client
@@ -38,6 +41,7 @@ router.get("/deliveryTime", async function (request, response) {
 
   response.send(data);
 });
+
 // ------------------------------------------- get shopDetails by low to high (API)  -------------------------------------------
 router.get("/LowToHigh", async function (request, response) {
   const data = await client
@@ -49,6 +53,7 @@ router.get("/LowToHigh", async function (request, response) {
 
   response.send(data);
 });
+
 // ------------------------------------------- get shopDetails by high to low (API)  -------------------------------------------
 router.get("/HighToLow", async function (request, response) {
   const data = await client
@@ -60,6 +65,7 @@ router.get("/HighToLow", async function (request, response) {
 
   response.send(data);
 });
+
 // ------------------------------------------- get shopDetails by Rating (API)  -------------------------------------------
 router.get("/Rating", async function (request, response) {
   const data = await client
@@ -71,6 +77,7 @@ router.get("/Rating", async function (request, response) {
 
   response.send(data);
 });
+
 // ------------------------------------------- create data (API) -------------------------------------------
 router.post("/createData", express.json(), async function (request, response) {
   const data = request.body;
@@ -81,6 +88,7 @@ router.post("/createData", express.json(), async function (request, response) {
 
   response.send(result);
 });
+
 // ------------------------------------------- update shop data (API) --------------------------------------------
 router.put("/shopId/:id", express.json(), async function (request, response) {
   const { id } = request.params;
@@ -93,12 +101,14 @@ router.put("/shopId/:id", express.json(), async function (request, response) {
 
   response.send(result);
 });
+
 // ------------------------------------------ delete All data (API) --------------------------------------------
 router.delete("/deleteAll", async function (request, response) {
   const result = await client.db("Swiggy").collection("product").deleteMany({});
 
   response.send(result);
 });
+
 // ------------------------------------------ delete one shop data (API) --------------------------------------------
 router.delete("/delete/:shopName", async function (request, response) {
   const { shopName } = request.params;
